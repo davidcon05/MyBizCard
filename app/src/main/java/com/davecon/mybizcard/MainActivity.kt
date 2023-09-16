ee74115d20481ed0819ca6ff6fd5d1a7bc7caf7c
David Contreras <david.b.contreras@gmail.com>
2023-09-15 22:17:02 -0400

    implement headshot component

--- app/src/main/java/com/davecon/mybizcard/MainActivity.kt
+++ app/src/main/java/com/davecon/mybizcard/MainActivity.kt
@@ -3,25 +3,26 @@ package com.davecon.mybizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.davecon.mybizcard.ui.theme.MyBizCardTheme
@@ -45,20 +46,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
     ) {
-        Card(modifier = Modifier
-            .padding(16.dp)
-            .width(200.dp)
-            .height(392.dp),
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .width(200.dp)
+                .height(392.dp),
             shape = RoundedCornerShape(CornerSize(16.dp)),
             //colors = CardDefaults.cardColors(containerColor = Color.LightGray),
             elevation = CardDefaults.cardElevation(8.dp),
 
-        ) {
-            Text(text = "Hello World!")
+            ) {
+
+            Surface(
+                modifier = Modifier
+                    .size(160.dp, 160.dp)
+                    .padding(8.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.Transparent),
+                shadowElevation = 8.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.headshot),
+                    contentDescription = "My headshot image",
+                    modifier = Modifier.size(120.dp)
+                )
+            }
         }
 
     }
