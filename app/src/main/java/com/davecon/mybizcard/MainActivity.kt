5f180880420669f10f7581cae4840ba268f954ec
David Contreras <david.b.contreras@gmail.com>
2023-09-15 21:54:01 -0400

    implement modifiers for card

--- app/src/main/java/com/davecon/mybizcard/MainActivity.kt
+++ app/src/main/java/com/davecon/mybizcard/MainActivity.kt
@@ -6,12 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.davecon.mybizcard.ui.theme.MyBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +49,17 @@ fun CreateBizCard() {
         .fillMaxWidth()
         .fillMaxHeight()
     ) {
+        Card(modifier = Modifier
+            .padding(16.dp)
+            .width(200.dp)
+            .height(392.dp),
+            shape = RoundedCornerShape(CornerSize(16.dp)),
+            colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+            elevation = CardDefaults.cardElevation(4.dp),
+
+        ) {
+            Text(text = "Hello World!")
+        }
 
     }
 }
