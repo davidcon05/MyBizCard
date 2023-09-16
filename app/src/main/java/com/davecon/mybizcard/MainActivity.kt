e005eff2f7a64b146ead9a290f18d004945bc0e6
David Contreras <david.b.contreras@gmail.com>
2023-09-16 13:01:16 -0400

    completed sections for personal info and button implementation, working on implementing a button that links out to github

--- app/src/main/java/com/davecon/mybizcard/MainActivity.kt
+++ app/src/main/java/com/davecon/mybizcard/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.davecon.mybizcard
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,17 +20,27 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import android.net.Uri
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType.Companion.Uri
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import com.davecon.mybizcard.ui.theme.MyBizCardTheme
+import com.google.firebase.inappmessaging.model.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +66,7 @@ fun CreateBizCard() {
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
+
         Card(
             modifier = Modifier
                 .padding(16.dp)
@@ -58,28 +74,104 @@ fun CreateBizCard() {
                 .height(392.dp),
             shape = RoundedCornerShape(CornerSize(16.dp)),
             //colors = CardDefaults.cardColors(containerColor = Color.LightGray),
-            elevation = CardDefaults.cardElevation(8.dp),
+            elevation = CardDefaults.cardElevation(8.dp)
+        ) {
 
-            ) {
+            HeadShotSection()
 
-            Surface(
-                modifier = Modifier
-                    .size(160.dp, 160.dp)
-                    .padding(8.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp, Color.Transparent),
-                shadowElevation = 8.dp,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
-            ) {
+            Divider(modifier = Modifier.padding(16.dp), thickness = 1.dp, color = Color.Black)
 
-                Image(
-                    painter = painterResource(id = R.drawable.headshot),
-                    contentDescription = "My headshot image",
-                    modifier = Modifier.size(120.dp)
-                )
-            }
+            NameAndTitleSection()
+
+            PortfolioButton()
         }
 
+    }
+}
+
+@Composable
+fun HeadShotSection() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Surface(
+            modifier = Modifier
+                .size(160.dp, 160.dp)
+                .padding(8.dp),
+            shape = CircleShape,
+            border = BorderStroke(0.5.dp, Color.Transparent),
+            shadowElevation = 8.dp,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.headshot),
+                contentDescription = "My headshot image",
+                modifier = Modifier.size(120.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun NameAndTitleSection() {
+    Column(
+        modifier = Modifier
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Top,
+    ) {
+
+        Text(
+            "David Contreras",
+            style = MaterialTheme.typography.headlineMedium,
+            fontSize = 36.sp,
+            color = MaterialTheme.colorScheme.primary//colorResource(id = R.color.purple_500)
+        )
+
+        Text(
+            "Software Engineer",
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodyMedium,
+            fontSize = 24.sp,
+        )
+
+        Text(
+            "https://github.com/davidcon05",
+            style = MaterialTheme.typography.bodyMedium,
+            fontSize = 16.sp,
+        )
+    }
+}
+
+@Composable
+fun PortfolioButton() {
+    val url = "https://github.com/davidcon05"
+    val urlIntent = Intent(
+        Intent.ACTION_VIEW,
+        //Uri.(url)
+    )
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        Button(onClick = {
+            Log.d("tag", "URL IS " + url)
+            //startActivity(urlIntent)
+        }) {
+            Text(
+                "Portfolio",
+                style = MaterialTheme.typography.bodyMedium,
+                fontSize = 24.sp,
+            )
+        }
     }
 }
 
